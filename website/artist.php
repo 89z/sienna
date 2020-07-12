a583df5bb76a675db6d713170b0ae59f8fc0ebbc
Steven Penny <svnpenn+nu8@gmail.com>
2020-07-12 18:18:23 -0500

    +       "Confield": {

--- website/artist.php
+++ website/artist.php
@@ -10,22 +10,24 @@ $s_artist = $_GET['a'];
 </head>
 <body>
    <header>
-      <div>
-         <a href="..">Up</a>
-      </div>
+      <a href="..">Up</a>
+      <?= '<h1>' . $s_artist . '</h1>' ?>
    </header>
-   <main>
+   <table>
 <?php
 $s_get = file_get_contents('../json/' . $s_file);
 $o_get = json_decode($s_get);
 foreach ($o_get->$s_artist as $s_album => $o_album) {
+   $s_date = $o_album->{'@date'};
 echo <<<eof
-<div>
-   <a href="/album.php?f=$s_file&a=$s_artist&r=$s_album">$s_album</a>
-</div>
-
+<tr>
+   <td>$s_date</td>
+   <td>
+      <a href="/album.php?f=$s_file&a=$s_artist&r=$s_album">$s_album</a>
+   </td>
+</tr>
 eof;
 }
 ?>
-   </main>
+   </table>
 </body>
