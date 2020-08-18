9a0f4c685f9e126d6fc310a10b044547705d390e
Steven Penny <svnpenn+nu8@gmail.com>
2020-08-18 07:36:47 -0500

    new file:   sc-insert.php

---
+++ sc-insert.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types = 1);
+
+extension_loaded('openssl') or die('openssl');
+require 'lib-r64.php';
+
+if ($argc != 2) {
+   echo "sc-insert.php <URL>\n";
+   exit(1);
+}
+
+$s_url = $argv[1];
+$s_get = file_get_contents($s_url);
+$s_dec = html_entity_decode($s_get);
+
+# track
+preg_match('!/tracks/([^"]*)"!', $s_dec, $a_track);
+$s_id_2 = $a_track[1];
+
+# img
+preg_match('!/artworks-([^.]*)-t500x500\.!', $s_dec, $a_img);
+$s_id_3 = $a_img[1];
+
+# year
+preg_match('/ pubdate>(\d{4})-/', $s_dec, $a_year);
+$s_year = $a_year[1];
+$n_year = (int)($s_year);
+
+# title
+preg_match('/<title>([^|]*) by ([^|]*) \|/', $s_dec, $a_title);
+$s_title = $a_title[2] . ' - ' . $a_title[1];
+
+# time
+$n_id_1 = time();
+$s_id_1 = r64_encode($n_id_1);
+
+# print
+$a_rec = [$s_id_1, $n_year, 's/' . $s_id_2 . '/' . $s_id_3, $s_title];
+$s_json = json_encode($a_rec, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+echo $s_json, ",\n";
