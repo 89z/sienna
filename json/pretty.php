10e9bdfc11074a5f9eeeae953a390750455f3162
Steven Penny <svnpenn+nu8@gmail.com>
2020-09-06 21:11:28 -0500

    new file:   json/pretty.php

---
+++ json/pretty.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types = 1);
+error_reporting(E_ALL);
+
+if ($argc != 2) {
+   echo "pretty.php <file>";
+   exit(1);
+}
+
+$s_file = $argv[1];
+$s_get = file_get_contents($s_file);
+$o_json = json_decode($s_get);
+
+if ($o_json == null) {
+   echo "invalid JSON\n";
+   exit(1);
+}
+
+$n = JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE;
+$s_json = json_encode($o_json, $n);
+file_put_contents($s_file, $s_json . "\n");
