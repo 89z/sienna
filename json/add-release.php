09d98cd36245b89f1fe10241fff0f555dd824441
Steven Penny <srpen6@gmail.com>
2020-12-06 10:26:23 -0600

    renamed:    json/0-A.json -> json/[0-A].json

--- json/add-release.php
+++ json/add-release.php
@@ -3,120 +3,105 @@ declare(strict_types = 1);
 extension_loaded('curl') or die('curl');
 extension_loaded('openssl') or die('openssl');
 
-if (! key_exists('f', $_GET)) {
-   echo "localhost:10/add-release.php?f=H-J.json&a=Harold Budd";
+if ($argc != 3) {
+   echo "add-release.php <artist> <file>\n";
    exit(1);
 }
 
-$file_s = $_GET['f'];
-$artist_s = $_GET['a'];
+$artist_s = $argv[1];
+$file_s = $argv[2];
 
 # local albums
-$s_json = file_get_contents($file_s);
-$o_local = json_decode($s_json);
-$s_arid = $o_local->$artist_s->{'@mb'};
-$m_local = si_color($o_local->$artist_s);
+$json_s = file_get_contents($file_s);
+$local_o = json_decode($json_s);
+$arid_s = $local_o->$artist_s->{'@mb'};
+$local_m = si_color($local_o->$artist_s);
 
-function si_color(object $o_artist): array {
-   foreach ($o_artist as $s_album => $o_album) {
-      if ($s_album[0] == '@') {
+function si_color(object $artist_o): array {
+   foreach ($artist_o as $album_s => $o_album) {
+      if ($album_s[0] == '@') {
          continue;
       }
-      $b_good = false;
-      $b_done = true;
-      foreach ($o_album as $s_track => $s_rate) {
-         if ($s_track == '@id') {
-            $m_local[$s_album] = 'black';
+      $good_b = false;
+      $done_b = true;
+      foreach ($o_album as $track_s => $rate_s) {
+         if ($track_s == '@id') {
+            $local_m[$album_s] = 'black';
             continue 2;
          }
-         if ($s_rate == 'good') {
-            $b_good = true;
+         if ($rate_s == 'good') {
+            $good_b = true;
          }
-         if ($s_rate == '') {
-            $b_done = false;
+         if ($rate_s == '') {
+            $done_b = false;
          }
       }
-      if ($b_good && $b_done) {
-         $m_local[$s_album] = 'green';
+      if ($good_b && $done_b) {
+         $local_m[$album_s] = 'green';
       }
-      if ($b_good && ! $b_done) {
-         $m_local[$s_album] = 'lightgreen';
+      if ($good_b && ! $done_b) {
+         $local_m[$album_s] = 'lightgreen';
       }
-      if (! $b_good && $b_done) {
-         $m_local[$s_album] = 'red';
+      if (! $good_b && $done_b) {
+         $local_m[$album_s] = 'red';
       }
-      if (! $b_good && ! $b_done) {
-         $m_local[$s_album] = 'lightred';
+      if (! $good_b && ! $done_b) {
+         $local_m[$album_s] = 'lightred';
       }
    }
-   return $m_local;
+   return $local_m;
 }
 
 # remote albums
-function mb_albums(string $s_arid): array {
-   $m_q['artist'] = $s_arid;
-   $m_q['fmt'] = 'json';
-   $m_q['inc'] = 'release-groups';
-   $m_q['limit'] = 100;
-   $m_q['offset'] = 0;
-   $m_q['status'] = 'official';
-   $m_q['type'] = 'album';
-   $m_remote = [];
-   $r_c = curl_init();
-   curl_setopt($r_c, CURLOPT_RETURNTRANSFER, true);
-   curl_setopt($r_c, CURLOPT_USERAGENT, 'anonymous');
+function mb_albums(string $arid_s): array {
+   $query_m['artist'] = $arid_s;
+   $query_m['fmt'] = 'json';
+   $query_m['inc'] = 'release-groups';
+   $query_m['limit'] = 100;
+   $query_m['offset'] = 0;
+   $query_m['status'] = 'official';
+   $query_m['type'] = 'album';
+   $remote_m = [];
+   $url_r = curl_init();
+   curl_setopt($url_r, CURLOPT_RETURNTRANSFER, true);
+   curl_setopt($url_r, CURLOPT_USERAGENT, 'anonymous');
    while (true) {
       # part 1
-      $s_q = http_build_query($m_q);
-      $s_url = 'https://musicbrainz.org/ws/2/release?' . $s_q;
-      curl_setopt($r_c, CURLOPT_URL, $s_url);
-      print $s_url . "\n";
+      $query_s = http_build_query($query_m);
+      $url_s = 'https://musicbrainz.org/ws/2/release?' . $query_s;
+      curl_setopt($url_r, CURLOPT_URL, $url_s);
+      echo $url_s, "\n";
       # part 2
-      $s_json = curl_exec($r_c);
+      $json_s = curl_exec($url_r);
       # part 3
-      $o_remote = json_decode($s_json);
-      foreach ($o_remote->releases as $o_re) {
+      $remote_o = json_decode($json_s);
+      foreach ($remote_o->releases as $o_re) {
          $o_rg = $o_re->{'release-group'};
          $a_sec = $o_rg->{'secondary-types'};
          if (count($a_sec) > 0) {
             continue;
          }
-         if (array_key_exists($o_rg->title, $m_remote)) {
+         if (array_key_exists($o_rg->title, $remote_m)) {
             continue;
          }
-         $m_remote[$o_rg->title] = $o_rg->{'first-release-date'};
+         $remote_m[$o_rg->title] = $o_rg->{'first-release-date'};
       }
-      $m_q['offset'] += $m_q['limit'];
-      if ($m_q['offset'] >= $o_remote->{'release-count'}) {
+      $query_m['offset'] += $query_m['limit'];
+      if ($query_m['offset'] >= $remote_o->{'release-count'}) {
          break;
       }
    }
-   return $m_remote;
+   return $remote_m;
 }
 
-$m_remote = mb_albums($s_arid);
-arsort($m_remote);
-?>
-<head>
-   <link rel="stylesheet" href="/sienna.css">
-</head>
-<body>
-   <header>
-      <a href="/artist?<?= http_build_query($_GET) ?>">Up</a>
-      <h1><?= $artist_s ?></h1>
-   </header>
-   <table>
-<?php
-foreach ($m_remote as $s_title => $s_date) {
-   printf('<tr><td>%s</td>', $s_date);
-   if (array_key_exists($s_title, $m_local)) {
-      $s_class = $m_local[$s_title];
-      printf('<td style="background:%s">%s', $s_class, $s_title);
+$remote_m = mb_albums($arid_s);
+arsort($remote_m);
+foreach ($remote_m as $title_s => $date_s) {
+   echo $date_s, "\t";
+   if (array_key_exists($title_s, $local_m)) {
+      $class_s = $local_m[$title_s];
+      printf('<td style="background:%s">%s', $class_s, $title_s);
    } else {
-      printf('<td>%s', $s_title);
+      printf('<td>%s', $title_s);
    }
-   print '</td></tr>';
 }
-?>
-   </table>
-</body>
