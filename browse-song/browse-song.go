115fe66b0961bd851dc2e48e7a182b2b5271b490
Steven Penny <srpen6@gmail.com>
2020-12-21 00:01:59 -0600

    +   query_s := fmt.Sprintf(`intitle:"%v" intext:"%v topic"`, song_s, band_s)

--- browse-song/browse-song.go
+++ browse-song/browse-song.go
@@ -17,13 +17,12 @@ func main() {
    band_s := os.Args[1]
    song_s := os.Args[2]
    browse_s := os.Getenv("BROWSER")
+   query_s := fmt.Sprintf(`intitle:"%v" intext:"%v topic"`, song_s, band_s)
+   m := url.Values{}
+   m.Set("q", query_s)
+   url_s := "youtube.com/results?" + m.Encode()
 
-   query_m := url.Values{"q": []string{
-      fmt.Sprintf(`intext:"%v - topic" intitle:"%v"`, band_s, song_s),
-   }}
-   query_s := query_m.Encode()
-
-   e := exec.Command(browse_s, "youtube.com/results?" + query_s).Start()
+   e := exec.Command(browse_s, url_s).Start()
    if e != nil {
       log.Fatal(e)
    }
