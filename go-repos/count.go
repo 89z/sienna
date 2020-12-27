b0269ebfe442ec74c87df1ad4e5ed3b9a155d4dc
Steven Penny <srpen6@gmail.com>
2020-12-27 15:34:22 -0600

    +   "sort"

--- go-repos/count.go
+++ go-repos/count.go
@@ -5,13 +5,22 @@ import (
    "golang.org/x/build/repos"
    "net/http"
    "sienna/assert"
+   "sort"
 )
+
+type Repo struct {
+   Count int
+   Path string
+}
+
+var repo_a []Repo
 
 func Count() error {
    for repo_s, repo_o := range repos.ByImportPath {
       if ! repo_o.ShowOnDashboard() {
          continue
       }
+      println(repo_s)
       url_s := "https://api.godoc.org/search?q=" + repo_s + "/"
       get_o, e := http.Get(url_s)
       if e != nil {
@@ -21,7 +30,13 @@ func Count() error {
       json.NewDecoder(get_o.Body).Decode(&get_m)
       result_a := get_m.A("results")
       len_n := len(result_a)
-      println(len_n, repo_s)
+      repo_a = append(repo_a, Repo{len_n, repo_s})
+   }
+   sort.Slice(repo_a, func(n, n2 int) bool {
+      return repo_a[n].Count < repo_a[n2].Count
+   })
+   for _, o := range repo_a {
+      println(o.Count, o.Path)
    }
    return nil
 }
--- go-repos/download.go
+++ go-repos/download.go
@@ -1,5 +1,33 @@
 package main
-import "strings"
+
+import (
+   "encoding/json"
+   "golang.org/x/build/repos"
+   "net/http"
+   "sienna/assert"
+   "strings"
+)
+
+var justify_m = map[string]string{
+   "golang.org/x/arch": "",
+   "golang.org/x/benchmarks": "",
+   "golang.org/x/blog": "",
+   "golang.org/x/build": "",
+   "golang.org/x/crypto": "",
+   "golang.org/x/exp": "golang.org/x/exp/utf8string",
+   "golang.org/x/image": "",
+   "golang.org/x/mobile": "",
+   "golang.org/x/mod": "",
+   "golang.org/x/net": "golang.org/x/net/html",
+   "golang.org/x/oauth2": "",
+   "golang.org/x/perf": "",
+   "golang.org/x/sync": "",
+   "golang.org/x/sys": "golang.org/x/sys/windows",
+   "golang.org/x/text": "golang.org/x/text/cases",
+   "golang.org/x/time": "",
+   "golang.org/x/tools": "",
+   "golang.org/x/website": "",
+}
 
 func BadPath(s string) bool {
    if ! strings.HasPrefix(s, "golang.org/x/") {
@@ -14,26 +42,33 @@ func BadPath(s string) bool {
    return false
 }
 
-var bad_repo = map[string]bool{
-   "golang.org/x/arch": true,
-   "golang.org/x/benchmarks": true,
-   "golang.org/x/blog": true,
-   "golang.org/x/build": true,
-   "golang.org/x/crypto": true,
-   "golang.org/x/debug": true,
-   "golang.org/x/mobile": true,
-   "golang.org/x/mod": true,
-   "golang.org/x/oauth2": true,
-   "golang.org/x/perf": true,
-   "golang.org/x/review": true,
-   "golang.org/x/sync": true,
-   "golang.org/x/talks": true,
-   "golang.org/x/term": true,
-   "golang.org/x/time": true,
-   "golang.org/x/tools": true,
-   "golang.org/x/website": true,
-}
-
 func Download() error {
+   count_n := 0
+   for repo_s, repo_o := range repos.ByImportPath {
+      if ! repo_o.ShowOnDashboard() {
+         continue
+      }
+      if justify_m[repo_s] == "" {
+         continue
+      }
+      url_s := "https://api.godoc.org/search?q=" + repo_s + "/"
+      println(url_s)
+      get_o, e := http.Get(url_s)
+      if e != nil {
+         return e
+      }
+      get_m := assert.Map{}
+      json.NewDecoder(get_o.Body).Decode(&get_m)
+      result_a := get_m.A("results")
+      for n := range result_a {
+         path_s := result_a.M(n).S("path")
+         if BadPath(path_s) {
+            continue
+         }
+         println("  ", path_s)
+         count_n++
+      }
+   }
+   println(count_n)
    return nil
 }
