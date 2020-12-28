4ded9aeab37233814daf2dee51cb698827258f95
Steven Penny <srpen6@gmail.com>
2020-12-28 07:58:11 -0600

    new file:   go-repos/.gitignore

--- go-repos/download.go
+++ go-repos/download.go
@@ -4,51 +4,39 @@ import (
    "encoding/json"
    "golang.org/x/build/repos"
    "net/http"
+   "os"
    "sienna/assert"
    "strings"
 )
 
-var justify_m = map[string]string{
-   "golang.org/x/arch": "",
-   "golang.org/x/benchmarks": "",
-   "golang.org/x/blog": "",
-   "golang.org/x/build": "",
-   "golang.org/x/crypto": "",
-   "golang.org/x/exp": "golang.org/x/exp/utf8string",
-   "golang.org/x/image": "",
-   "golang.org/x/mobile": "",
-   "golang.org/x/mod": "",
-   "golang.org/x/net": "golang.org/x/net/html",
-   "golang.org/x/oauth2": "",
-   "golang.org/x/perf": "",
-   "golang.org/x/sync": "",
-   "golang.org/x/sys": "golang.org/x/sys/windows",
-   "golang.org/x/text": "golang.org/x/text/cases",
-   "golang.org/x/time": "",
-   "golang.org/x/tools": "",
-   "golang.org/x/website": "",
+var bad_repo = map[string]bool{
+   "golang.org/x/build": true,
+   "golang.org/x/crypto": true,
+   "golang.org/x/oauth2": true,
+   "golang.org/x/tools": true,
 }
 
-func BadPath(s string) bool {
-   if ! strings.HasPrefix(s, "golang.org/x/") {
-      return true
+func Copy(source, dest string) (int64, error) {
+   println(source)
+   get_o, e := http.Get(source)
+   if e != nil {
+      return 0, e
    }
-   if strings.Contains(s, "/cmd/") {
-      return true
+   create_o, e := os.Create(dest)
+   if e != nil {
+      return 0, e
    }
-   if strings.Contains(s, "/vendor/") {
-      return true
-   }
-   return false
+   return create_o.ReadFrom(get_o.Body)
 }
 
 func Download() error {
-   count_n := 0
+   os.Mkdir("x", os.ModeDir)
+   os.Chdir("x")
    for repo_s, repo_o := range repos.ByImportPath {
       if ! repo_o.ShowOnDashboard() {
          continue
       }
-      if justify_m[repo_s] == "" {
+      if bad_repo[repo_s] {
          continue
       }
       url_s := "https://api.godoc.org/search?q=" + repo_s + "/"
@@ -58,17 +46,26 @@ func Download() error {
          return e
       }
       get_m := assert.Map{}
-      json.NewDecoder(get_o.Body).Decode(&get_m)
+      e = json.NewDecoder(get_o.Body).Decode(&get_m)
+      if e != nil {
+         return e
+      }
       result_a := get_m.A("results")
       for n := range result_a {
          path_s := result_a.M(n).S("path")
-         if BadPath(path_s) {
+         if ! strings.HasPrefix(path_s, "golang.org/x/") {
             continue
          }
-         println("  ", path_s)
-         count_n++
+         path_a := strings.Split(path_s, "/")
+         if len(path_a) > 4 {
+            continue
+         }
+         dest := strings.ReplaceAll(path_s, "/", "-")
+         _, e = Copy("https://pkg.go.dev/" + path_s, dest + ".html")
+         if e != nil {
+            return e
+         }
       }
    }
-   println(count_n)
    return nil
 }
