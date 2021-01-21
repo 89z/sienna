3c70674a79f45112d6fe4c1000b6484c8325a471
Steven Penny <srpen6@gmail.com>
2021-01-20 22:43:25 -0600

    deleted:    sienna.go

--- rust-installer/installer.go
+++ rust-installer/installer.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+   "github.com/89z/x"
+   "github.com/89z/x/toml"
    "github.com/mholt/archiver/v3"
    "log"
+   "os"
+   "path/filepath"
 )
 
 const channel = "https://static.rust-lang.org/dist/channel-rust-stable.toml"
+
+var packages = []string{
+   "pkg.cargo.target.x86_64-pc-windows-gnu",
+   "pkg.rust-std.target.x86_64-pc-windows-gnu",
+   "pkg.rustc.target.x86_64-pc-windows-gnu",
+}
 
 func check(e error) {
    if e != nil {
@@ -18,4 +28,28 @@ func unarchive(file, dir string) error {
    println("EXTRACT", file)
    xz := archiver.TarXz{Tar: tar}
    return xz.Unarchive(file, dir)
+}
+
+func main() {
+   user, e := os.UserCacheDir()
+   check(e)
+   e = os.Chdir(filepath.Join(user, "rust"))
+   check(e)
+   dist := filepath.Base(channel)
+   if ! x.IsFile(dist) {
+      _, e = x.HttpCopy(channel, dist)
+      check(e)
+   }
+   manifest, e := toml.LoadFile(dist)
+   check(e)
+   for _, pack := range packages {
+      url := manifest.M(pack).S("xz_url")
+      base := filepath.Base(url)
+      if ! x.IsFile(base) {
+         _, e = x.HttpCopy(url, base)
+         check(e)
+      }
+      e = unarchive(base, `C:\rust`)
+      check(e)
+   }
 }
--- rust-installer/readme.md
+++ rust-installer/readme.md
@@ -1,3 +1,12 @@
 # Rust
 
+~~~toml
+[pkg.cargo.target.x86_64-pc-windows-gnu]
+available = true
+url = "https://static.rust-lang.org/dist/2020-12-31/cargo-1.49.0-x86_64-pc-windows-gnu.tar.gz"
+hash = "b361f0410813d2c4acd2389a644b1c5678a5934b15d61c255dfc11992980269b"
+xz_url = "https://static.rust-lang.org/dist/2020-12-31/cargo-1.49.0-x86_64-pc-windows-gnu.tar.xz"
+xz_hash = "01676e45eeaa0ab138279471feadabcf621c622c6a66347e0cfd8f5710d6cada"
+~~~
+
 <https://github.com/rust-lang/rustup/issues/2569>
