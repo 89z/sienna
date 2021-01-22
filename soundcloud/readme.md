9922ac8251916d4aca3169edcb306581d8d4ac05
Steven Penny <srpen6@gmail.com>
2021-01-21 23:18:57 -0600

    new file:   soundcloud/readme.md

---
+++ soundcloud/readme.md
@@ -0,0 +1,4 @@
+# SoundCloud
+
+- https://soundcloud.com/fedecrossover/disclosure-ftsam-smith-latch-daniela-andrade-cover-fede-remix
+- https://soundcloud.com/four-tet/burial-four-tet-nova
--- soundcloud/soundcloud-insert.go
+++ soundcloud/soundcloud-insert.go
@@ -1,24 +1,29 @@
 package main
 
 import (
+   "encoding/json"
    "github.com/89z/x"
+   "io/ioutil"
    "log"
    "os"
    "path"
+   "regexp"
    "strconv"
+   "time"
 )
 
-func encode36(x int) string {
-   n := int64(x)
-   return strconv.FormatInt(n, 36)
+func check(e error) {
+   if e != nil {
+      log.Fatal(e)
+   }
 }
 
-func findSubmatch(pat string, sub []byte) []byte {
-   a := regexp.MustCompile(pat).FindSubmatch(sub)
+func findSubmatch(re string, input []byte) string {
+   a := regexp.MustCompile(re).FindSubmatch(input)
    if len(a) < 2 {
-      return []byte{}
+      return ""
    }
-   return a[1]
+   return string(a[1])
 }
 
 func main() {
@@ -30,29 +35,19 @@ func main() {
    dest := path.Base(url) + ".html"
    if ! x.IsFile(dest) {
       _, e := x.HttpCopy(url, dest)
-      if e != nil {
-         log.Fatal(e)
-      }
+      check(e)
    }
-   y, e := ioutil.ReadFile(dest)
-   if e != nil {
-      log.Fatal(e)
-   }
-   /*
-   preg_match('!/tracks/([^"]*)"!', $dec_s, $track_a);
-   $dec_s = html_entity_decode($get_s);
-   $audio_s = $track_a[1];
-   preg_match('!/artworks-([^.]*)-t500x500\.!', $dec_s, $img_a);
-   $video_s = $img_a[1];
-   preg_match('/ pubdate>(\d{4})-/', $dec_s, $year_a);
-   $year_s = $year_a[1];
-   $year_n = (int)($year_s);
-   preg_match('/<title>([^|]*) by ([^|]*) \|/', $dec_s, $title_a);
-   $title_s = $title_a[2] . ' - ' . $title_a[1];
-   $date_n = time();
-   $date_s = encode36($date_n);
-   $rec_a = [$date_s, $year_n, 's/' . $audio_s . '/' . $video_s, $title_s];
-   $json_s = json_encode($rec_a, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-   echo $json_s, ",\n";
-   */
+   input, e := ioutil.ReadFile(dest)
+   check(e)
+   audio := findSubmatch(`/tracks/([^"]*)"`, input)
+   video := findSubmatch("-([^-]+-[^-]+)-t500x500.jpg", input)
+   title := findSubmatch("<title>([^|]+) by ", input)
+   year_s := findSubmatch(` pubdate>(\d{4})-`, input)
+   year_n, e := strconv.Atoi(year_s)
+   check(e)
+   date_n := time.Now().Unix()
+   date_s := strconv.FormatInt(date_n, 36)
+   rec := x.Slice{date_s, year_n, "s/" + audio + "/" + video, title}
+   e = json.NewEncoder(os.Stdout).Encode(rec)
+   check(e)
 }
