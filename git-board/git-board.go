f9d549a02cb6c240fa099ad1bc964529fcfdaf7c
Steven Penny <srpen6@gmail.com>
2021-01-23 18:37:43 -0600

    new file:   git-board/go.mod

--- git-board/git-board.go
+++ git-board/git-board.go
@@ -1,52 +1,74 @@
-<?php
-declare(strict_types = 1);
+package main
 
-function color_cyan(string $s): string {
-   return "\e[1;36m" . $s . "\e[m";
-}
+import (
+   "bufio"
+   "fmt"
+   "github.com/89z/x"
+   "log"
+   "strings"
+   "time"
+)
 
-function color_green(string $s): string {
-   return "\e[1;32m" . $s . "\e[m";
-}
+const min = 64
 
-function color_red(string $s): string {
-   return "\e[1;31m" . $s . "\e[m";
-}
-
-function color_f(bool $test_b, string $mesg_s): string {
-   return $test_b ? color_green($mesg_s) : color_red($mesg_s);
-}
-
-$min_n = 64;
-$cha_n = $add_n = $rem_n = 0;
-system('git add .');
-
-if (is_file('config.toml')) {
-   exec('git diff --cached --numstat :!docs', $tab_a);
-} else {
-   exec('git diff --cached --numstat', $tab_a);
-}
-
-foreach ($tab_a as $row_s) {
-   $cha_n++;
-   $row_a = sscanf($row_s, "%d\t%d");
-   if (str_starts_with($row_s, '-')) {
-      continue;
+func check(e error) {
+   if e != nil {
+      log.Fatal(e)
    }
-   $add_n += $row_a[0];
-   $rem_n += $row_a[1];
 }
 
-echo 'minimum: ', $min_n, "\n";
-echo color_f($cha_n >= $min_n, 'changed files: ' . $cha_n), "\n";
-echo color_f($add_n >= $min_n, 'additions: ' . $add_n), "\n";
-echo color_f($rem_n >= $min_n, 'deletions: ' . $rem_n), "\n";
-echo "\n";
+func color(test bool, key string, value interface{}) {
+   message := fmt.Sprint(key, ": ", value)
+   if test {
+      fmt.Println(x.ColorGreen(message))
+   } else {
+      fmt.Println(x.ColorRed(message))
+   }
+}
 
-$then_s = shell_exec('git log -1 --date=short --format=%cd');
-$zone_o = new DateTimeZone('America/Chicago');
-$date_o = new DateTime(timezone: $zone_o);
-$now_s = $date_o->format('Y-m-d');
+func diff() (*bufio.Scanner, error) {
+   if x.IsFile("config.toml") {
+      return x.Popen("git", "diff", "--cached", "--numstat", ":!docs")
+   }
+   return x.Popen("git", "diff", "--cached", "--numstat")
+}
 
-echo 'last commit date: ', $then_s;
-echo color_f($now_s > $then_s, 'current date: ' . $now_s), "\n";
+func equalDate(value string) (bool, error) {
+   t, e := time.Parse(time.RFC3339, value)
+   if e != nil {
+      return false, e
+   }
+   s := time.RFC3339[:10]
+   return t.Format(s) == time.Now().Format(s), nil
+}
+
+func main() {
+   e := x.System("git", "add", ".")
+   check(e)
+   stat, e := diff()
+   check(e)
+   var add, del, totAdd, totCha, totDel int
+   for stat.Scan() {
+      totCha++
+      text := stat.Text()
+      if strings.HasPrefix(text, "-") {
+         continue
+      }
+      fmt.Sscanf(text, "%v\t%v", &add, &del)
+      totAdd += add
+      totDel += del
+   }
+   fmt.Println("minimum:", min)
+   color(totCha >= min, "changed files", totCha)
+   color(totAdd >= min, "additions", totAdd)
+   color(totDel >= min, "deletions", totDel)
+   fmt.Println()
+   commit, e := x.Popen("git", "log", "--format=%cI")
+   check(e)
+   commit.Scan()
+   then := commit.Text()
+   fmt.Println("last commit date:", then)
+   equal, e := equalDate(then)
+   check(e)
+   color(equal, "current date:", time.Now())
+}
---
+++ git-board/go.mod
@@ -0,0 +1,5 @@
+module git-board
+
+go 1.15
+
+require github.com/89z/x v1.1.8
