9f4f1c8a21ab09ce352bedfc959369ae8e914428
Steven Penny <srpen6@gmail.com>
2021-01-23 21:27:10 -0600

    deleted:    car-insurance/car-insurance.css

--- car-insurance/car-insurance.go
+++ car-insurance/car-insurance.go
@@ -1,33 +1,28 @@
 package main
 
-const (
-   address1 = "1234 DELTA ECHO FOXTROT 567"
-   address2 = "GOLF, HO 89012"
-   agent_name = "QUEBEC ROMEO"
-   agent_phone = "(456) 789-0123"
-   company1 = "INDIA JULIET KILO LIMA"
-   company2 = "MIKE NOVEMBER OSCAR PAPA"
-   company_phone = "(345) 678-9012"
-   driver_name = "ALFA B CHARLIE"
-   make_model = "2016 TANGO UNIFORM"
-   policy_num = "45 SIE - 678901234"
-   vehicle_num = "VICTO56789R012345"
+import (
+   "fmt"
+   "time"
 )
 
-<!doctype html>
-<html lang="en">
-<head>
-   <link rel="stylesheet" href="cove.css">
-   <meta charset="utf-8">
-   <title>Car insurance</title>
-</head>
-<body>
-$date_o = new DateTime;
-$add_o = new DateInterval('P1M');
-for ($n = 12; $n > 0; $n--) {
-   $from_s = $date_o->format('Y-m-d');
-   $date_o->add($add_o);
-   $to_s = $date_o->format('Y-m-d');
+const (
+   agent = "QUEBEC ROMEO"
+   agentPhone = "(456) 789-0123"
+   company = "INDIA JULIET KILO LIMA"
+   companyDept = "MIKE NOVEMBER OSCAR PAPA"
+   companyPhone = "(345) 678-9012"
+   driver = "ALFA B CHARLIE"
+   driverAddr = "1234 DELTA ECHO FOXTROT 567"
+   driverCity = "GOLF, HO 89012"
+   policy = "45 SIE - 678901234"
+   vehicle = "2016 TANGO UNIFORM"
+   vehicleNum = "VICTO56789R012345"
+)
+
+func main() {
+   from := time.Now()
+   for n := 12; n > 0; n-- {
+      fmt.Println(from.Format(time.RFC3339[:10]))
+      from = from.AddDate(0, 1, 0)
+   }
 }
-</body>
-</html>
