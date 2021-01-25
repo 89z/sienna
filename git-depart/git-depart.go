c7332ac5c55346124c186f3fd3e7ee84f829b1c8
Steven Penny <srpen6@gmail.com>
2021-01-25 16:51:53 -0600

    deleted:    git-touch/main.go

--- git-depart/git-depart.go
+++ git-depart/git-depart.go
@@ -2,31 +2,24 @@ package main
 
 import (
    "github.com/89z/x"
-   "log"
    "os"
 )
 
-func check(e error) {
-   if e != nil {
-      log.Fatal(e)
-   }
-}
-
 func main() {
    e := x.System("git", "commit", "--verbose")
-   check(e)
+   x.Check(e)
    if x.IsFile("config.toml") {
       println(x.ColorGreen("remove docs"))
       os.RemoveAll("docs")
       println(x.ColorGreen("hugo"))
       e = x.System("hugo")
-      check(e)
+      x.Check(e)
       println(x.ColorGreen("git add"))
       e = x.System("git", "add", ".")
-      check(e)
+      x.Check(e)
       println(x.ColorGreen("git commit"))
       e = x.System("git", "commit", "--amend")
-      check(e)
+      x.Check(e)
    }
    println(x.ColorGreen("git push"))
 }
--- git-touch/git-touch.go
+++ git-touch/git-touch.go
@@ -1,22 +1,40 @@
 package main
 
 import (
-   "bufio"
-   "log"
-   "os/exec"
+   "github.com/89z/x"
+   "os"
+   "strconv"
+   "time"
 )
 
-func check(e error) {
-   if e != nil {
-      log.Fatal(e)
+func main() {
+   gitLs, e := x.Popen("git", "ls-files")
+   x.Check(e)
+   files := map[string]bool{}
+   for gitLs.Scan() {
+      files[gitLs.Text()] = true
    }
-}
-
-func popen(name string, arg ...string) (*bufio.Scanner, error) {
-   cmd := exec.Command(name, arg...)
-   pipe, e := cmd.StdoutPipe()
-   if e != nil {
-      return nil, e
+   gitLog, e := x.Popen(
+      "git", "log", "-m",
+      "--name-only", "--relative", "--pretty=format:%ct", ".",
+   )
+   x.Check(e)
+   for len(files) > 0 {
+      gitLog.Scan()
+      sec, e := strconv.ParseInt(gitLog.Text(), 10, 64)
+      x.Check(e)
+      unix := time.Unix(sec, 0)
+      for gitLog.Scan() {
+         name := gitLog.Text()
+         if name == "" {
+            break
+         }
+         if ! files[name] {
+            continue
+         }
+         println(sec, "\t", name)
+         os.Chtimes(name, unix, unix)
+         delete(files, name)
+      }
    }
-   return bufio.NewScanner(pipe), cmd.Start()
 }
