8e9735d29c257f3f3551f9fd427eaa2503cdd08f
Steven Penny <srpen6@gmail.com>
2021-02-05 08:44:17 -0600

    deleted:    msys2-manager/main.go

--- msys2-manager/manager.go
+++ msys2-manager/manager.go
@@ -4,9 +4,10 @@ import (
    "bufio"
    "fmt"
    "github.com/89z/x"
+   "github.com/89z/x/extract"
    "io/ioutil"
    "os"
-   "path/filepath"
+   "path"
    "strings"
 )
 
@@ -31,7 +32,7 @@ func (m manager) getValue(pack, key string) (val []string, e error) {
       return nil, fmt.Errorf("%v %v", pack, key)
    }
    open, e := os.Open(
-      filepath.Join(m.Cache, name, "desc"),
+      path.Join(m.Cache, name, "desc"),
    )
    if e != nil {
       return
@@ -77,7 +78,7 @@ func (m manager) sync(tar string) error {
          return e
       }
       file := values[0]
-      archive := filepath.Join(m.Cache, file)
+      archive := path.Join(m.Cache, file)
       if ! x.IsFile(archive) {
          _, e := x.Copy(
             getRepo(file) + file, archive,
@@ -92,4 +93,78 @@ func (m manager) sync(tar string) error {
       }
    }
    return nil
+}
+
+
+
+func baseName(s, char string) string {
+   n := strings.IndexAny(s, char)
+   if n == -1 {
+      return s
+   }
+   return s[:n]
+}
+
+func getRepo(s string) string {
+   if s == "mingw64.db.tar.gz" || strings.HasPrefix(s, "mingw-w64-x86_64-") {
+      return "http://repo.msys2.org/mingw/x86_64/"
+   }
+   return "http://repo.msys2.org/msys/x86_64/"
+}
+
+func unarchive(source, dest string) error {
+   var tar extract.Tar
+   switch path.Ext(source) {
+   case ".zst":
+      return tar.Zst(source, dest)
+   case ".xz":
+      return tar.Xz(source, dest)
+   default:
+      return tar.Gz(source, dest)
+   }
+}
+
+func main() {
+   if len(os.Args) != 3 {
+      println(`synopsis:
+   msys2-manager <operation> <target>
+
+examples:
+   msys2-manager deps mingw-w64-x86_64-libgit2
+   msys2-manager sync git.txt`)
+      os.Exit(1)
+   }
+   target := os.Args[2]
+   install, e := x.NewInstall("msys64")
+   x.Check(e)
+   for _, each := range []string{"mingw64.db.tar.gz", "msys.db.tar.gz"} {
+      archive := path.Join(install.Cache, each)
+      if x.IsFile(archive) {
+         continue
+      }
+      _, e = x.Copy(
+         getRepo(each) + each, archive,
+      )
+      x.Check(e)
+      e = unarchive(archive, install.Cache)
+      x.Check(e)
+   }
+   man := manager{install}
+   if os.Args[1] == "sync" {
+      e = man.sync(target)
+      x.Check(e)
+      return
+   }
+   var packSet = map[string]bool{}
+   for packs := []string{target}; len(packs) > 0; packs = packs[1:] {
+      target := packs[0]
+      deps, e := man.getValue(target, "%DEPENDS%")
+      x.Check(e)
+      packs = append(packs, deps...)
+      if packSet[target] {
+         continue
+      }
+      println(target)
+      packSet[target] = true
+   }
 }
