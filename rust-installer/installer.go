7155ae0cb7a6cbd48f384d56391ce2a92a5b7a11
Steven Penny <srpen6@gmail.com>
2021-02-12 13:13:56 -0600

    +	github.com/89z/x v1.9.7

--- rust-installer/installer.go
+++ rust-installer/installer.go
@@ -5,8 +5,14 @@ import (
    "github.com/89z/x/extract"
    "github.com/pelletier/go-toml"
    "io/ioutil"
+   "log"
    "os"
-   "path/filepath"
+   "path"
+)
+
+const (
+   remote = "https://static.rust-lang.org/dist/channel-rust-stable.toml"
+   local = `C:\rust`
 )
 
 type distChannel struct{
@@ -20,41 +26,72 @@ type distChannel struct{
 type target struct{
    Target struct{
       X8664PcWindowsGnu struct{
-         Url string
+         XzUrl string `toml:"xz_url"`
       } `toml:"x86_64-pc-windows-gnu"`
    }
 }
 
-const (
-   source = "https://static.rust-lang.org/dist/channel-rust-stable.toml"
-   dest = `C:\rust`
-)
+type userCache struct {
+   dir string
+}
+
+func newUserCache(dir ...string) (userCache, error) {
+   cache, e := os.UserCacheDir()
+   if e != nil {
+      return userCache{}, e
+   }
+   dir = append([]string{cache}, dir...)
+   return userCache{
+      path.Join(dir...),
+   }, nil
+}
+
+func (c userCache) install(source string) error {
+   base := path.Base(source)
+   archive := path.Join(c.dir, base)
+   _, e := x.Copy(source, archive)
+   if os.IsExist(e) {
+      println(x.ColorCyan("Exist"), base)
+   } else if e != nil {
+      return e
+   }
+   tar := extract.Tar{Strip: 2}
+   println(x.ColorCyan("Xz"), base)
+   return tar.Xz(archive, local)
+}
+
+func (c userCache) unmarshal(v interface{}) error {
+   base := path.Base(remote)
+   dest := path.Join(c.dir, base)
+   _, e := x.Copy(remote, dest)
+   if os.IsExist(e) {
+      println(x.ColorCyan("Exist"), base)
+   } else if e != nil {
+      return e
+   }
+   data, e := ioutil.ReadFile(dest)
+   if e != nil {
+      return e
+   }
+   return toml.Unmarshal(data, v)
+}
 
 func main() {
-   cache, e := os.UserCacheDir()
-   x.Check(e)
-   cache = filepath.Join(cache, "rust")
-   channel := filepath.Join(
-      cache, filepath.Base(source),
-   )
-   _, e = x.Copy(source, channel)
-   x.Check(e)
-   data, e := ioutil.ReadFile(channel)
-   x.Check(e)
+   cache, e := newUserCache("sienna", "rust")
+   if e != nil {
+      log.Fatal(e)
+   }
    var dist distChannel
-   e = toml.Unmarshal(data, &dist)
-   x.Check(e)
-   tar := extract.Tar{Strip: 2}
+   e = cache.unmarshal(&dist)
+   if e != nil {
+      log.Fatal(e)
+   }
    for _, each := range []target{
       dist.Pkg.Cargo, dist.Pkg.RustStd, dist.Pkg.Rustc,
    } {
-      source := each.Target.X8664PcWindowsGnu.Url
-      archive := filepath.Join(
-         cache, filepath.Base(source),
-      )
-      _, e = x.Copy(source, archive)
-      x.Check(e)
-      e = tar.Gz(archive, dest)
-      x.Check(e)
+      e = cache.install(each.Target.X8664PcWindowsGnu.XzUrl)
+      if e != nil {
+         log.Fatal(e)
+      }
    }
 }
