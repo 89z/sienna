2d75b1e6af95c3673f0b7f2995f58a700457f884
Steven Penny <srpen6@gmail.com>
2021-03-26 00:01:41 -0500

    new file:   msys2/sync.go

--- msys2/manager.go
+++ msys2/manager.go
@@ -106,33 +106,3 @@ func newInstall(source url.URL, cache, dest string, base ...string) install {
    cache = filepath.Join(cache, filepath.Base(src))
    return install{src, cache, dest}
 }
-
-// FIXME
-func (m manager) sync(tar string) error {
-   open, e := os.Open(tar)
-   if e != nil {
-      return e
-   }
-   scan := bufio.NewScanner(open)
-   for scan.Scan() {
-      values, e := m.getValue(
-         scan.Text(), "%FILENAME%",
-      )
-      if e != nil {
-         return e
-      }
-      file := values[0]
-      archive := path.Join(m.Cache, file)
-      _, e = x.Copy(
-         getRepo(file) + file, archive,
-      )
-      if e != nil {
-         return e
-      }
-      e = unarchive(archive, m.Dest)
-      if e != nil {
-         return e
-      }
-   }
-   return nil
-}
