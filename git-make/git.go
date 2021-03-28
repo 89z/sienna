ba7dd0b3b75a83cc3e482c4f3534c5365364d1a6
Steven Penny <srpen6@gmail.com>
2021-03-28 13:52:15 -0500

    deleted:    rust-deps/deps.go

--- git-make/git.go
+++ git-make/git.go
@@ -16,9 +16,7 @@ const (
 
 func curlMake() error {
    cache, err := os.UserCacheDir()
-   if err != nil {
-      return err
-   }
+   if err != nil { return err }
    cache = filepath.Join(cache, "sienna", "curl")
    err = exec.Command(
       "git", "clone", "--branch", verCurl, "--depth", "1",
@@ -36,40 +34,30 @@ func curlMake() error {
 
 func gitCopy(root string) error {
    cache, err := os.UserCacheDir()
-   if err != nil {
-      return err
-   }
+   if err != nil { return err }
    cache = filepath.Join(cache, "sienna", "git")
    dest := root + "git"
    err = os.MkdirAll(
       filepath.Join(dest, "libexec", "git-core"), os.ModeDir,
    )
-   if err != nil {
-      return err
-   }
+   if err != nil { return err }
    err = os.MkdirAll(
       filepath.Join(dest, "share", "git-core", "templates"), os.ModeDir,
    )
-   if err != nil {
-      return err
-   }
+   if err != nil { return err }
    for _, each := range []string{"git.exe", "git-remote-https.exe"} {
       err = os.Link(
          filepath.Join(cache, each),
          filepath.Join(dest, "libexec", "git-core", each),
       )
-      if err != nil {
-         return err
-      }
+      if err != nil { return err }
    }
    return nil
 }
 
 func gitMake(root string) error {
    cache, err := os.UserCacheDir()
-   if err != nil {
-      return err
-   }
+   if err != nil { return err }
    cache = filepath.Join(cache, "sienna")
    cmd := exec.Command(
       "git", "clone", "--branch", verGit, "--depth", "1",
@@ -81,28 +69,20 @@ func gitMake(root string) error {
       cmd = exec.Command("git", "clean", "-d", "-f", "-x")
       cmd.Dir = filepath.Join(cache, "git")
       err = cmd.Run()
-      if err != nil {
-         return err
-      }
+      if err != nil { return err }
    }
    err = os.MkdirAll(
       root + filepath.Join("msys64", "tmp"), os.ModeDir,
    )
-   if err != nil {
-      return err
-   }
+   if err != nil { return err }
    err = os.Setenv("MSYSTEM", "MINGW64")
-   if err != nil {
-      return err
-   }
+   if err != nil { return err }
    paths := []string{
       root + filepath.Join("msys64", "mingw64", "bin"),
       root + filepath.Join("msys64", "usr", "bin"),
    }
    err = os.Setenv("PATH", strings.Join(paths, string(os.PathListSeparator)))
-   if err != nil {
-      return err
-   }
+   if err != nil { return err }
    cmd = exec.Command(
       "make", "-j", "8",
       "CFLAGS=-DCURL_STATICLIB",
