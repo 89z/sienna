2bda0c1b96745161ad1c6631e3b5642039badf03
Steven Penny <srpen6@gmail.com>
2021-04-19 17:19:19 -0500

    +func encode(name string, top, right, bottom, left int) error {

--- crop-image/crop-image.go
+++ crop-image/crop-image.go
@@ -2,56 +2,42 @@ package main
 
 import (
    "flag"
+   "fmt"
    "image"
    "image/jpeg"
-   "io"
    "log"
    "os"
    "path/filepath"
 )
 
-type imageRect struct {
-   bottom, left, right, top int
-}
-
-func (r imageRect) Encode(dst io.Writer, src io.Reader) error {
-   decode, e := jpeg.Decode(src)
-   if e != nil {
-      return e
-   }
+func encode(name string, top, right, bottom, left int) error {
+   in, e := os.Open(name)
+   if e != nil { return e }
+   defer in.Close()
+   out, e := os.Create("crop-" + filepath.Base(name))
+   if e != nil { return e }
+   defer out.Close()
+   decode, e := jpeg.Decode(in)
+   if e != nil { return e }
    bound := decode.Bounds()
-   rect := image.Rect(
-      r.left, r.top, bound.Max.X - r.right, bound.Max.Y - r.bottom,
-   )
-   return jpeg.Encode(
-      dst, decode.(*image.YCbCr).SubImage(rect), nil,
-   )
+   rect := image.Rect(left, top, bound.Max.X - right, bound.Max.Y - bottom)
+   fmt.Println(bound, rect)
+   return jpeg.Encode(out, decode.(*image.YCbCr).SubImage(rect), nil)
 }
 
 func main() {
-   var rect imageRect
-   flag.IntVar(&rect.bottom, "bottom", 0, "pixels")
-   flag.IntVar(&rect.left, "left", 0, "pixels")
-   flag.IntVar(&rect.right, "right", 0, "pixels")
-   flag.IntVar(&rect.top, "top", 0, "pixels")
+   var top, right, bottom, left int
+   flag.IntVar(&top, "top", 0, "pixels")
+   flag.IntVar(&right, "right", 0, "pixels")
+   flag.IntVar(&bottom, "bottom", 0, "pixels")
+   flag.IntVar(&left, "left", 0, "pixels")
    flag.Parse()
    if flag.NArg() != 1 {
-      println("crop-image [flags] <file>")
+      fmt.Println("crop-image [flags] <file>")
       flag.PrintDefaults()
       os.Exit(1)
    }
-   arg := flag.Arg(0)
-   open, e := os.Open(arg)
-   if e != nil {
-      log.Fatal(e)
-   }
-   create, e := os.Create(
-      "crop-" + filepath.Base(arg),
-   )
-   if e != nil {
-      log.Fatal(e)
-   }
-   e = rect.Encode(open, create)
+   e := encode(flag.Arg(0), top, right, bottom, left)
    if e != nil {
       log.Fatal(e)
    }
