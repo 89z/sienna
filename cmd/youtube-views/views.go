0cdd2b1a8c680bacf84b9bec072326ab8d0bcd95
Steven Penny <srpen6@gmail.com>
2021-04-30 14:05:24 -0500

    new file:   cmd/youtube-views/main.go

--- cmd/youtube-views/views.go
+++ cmd/youtube-views/views.go
@@ -13,34 +13,6 @@ import (
    "time"
 )
 
-func main() {
-   if len(os.Args) != 2 {
-      println("youtube-views <URL>")
-      os.Exit(1)
-   }
-   arg := os.Args[1]
-   addr, err := url.Parse(arg)
-   if err != nil {
-      panic(err)
-   }
-   id := addr.Query().Get("v")
-   video, err := youtube.NewVideo(id)
-   if err != nil {
-      panic(err)
-   }
-   date, err := video.PublishDate()
-   if err != nil {
-      panic(err)
-   }
-   view := time.Since(date).Hours() / 24 / 365
-   if view > 10_000_000 {
-      rosso.LogFail("Fail", view)
-   } else {
-      rosso.LogPass("Pass", view)
-   }
-}
-
-
 func youtubeResult(query string) (string, error) {
    req, e := http.NewRequest("GET", "http://youtube.com/results", nil)
    if e != nil { return "", e }
@@ -58,37 +30,4 @@ func youtubeResult(query string) (string, error) {
       return "", fmt.Errorf("%v", req.URL)
    }
    return find[1], nil
-}
-
-func main() {
-   if len(os.Args) != 2 {
-      println(`usage:
-musicbrainz-views <URL>
-
-examples:
-https://musicbrainz.org/release-group/d03bb6b1-d7b4-38ea-974e-847cbb31dca4
-https://musicbrainz.org/release/7a629d52-6a61-3ea1-a0a0-dd50bdef63b4`)
-      os.Exit(1)
-   }
-   arg := os.Args[1]
-   album, e := musicbrainz.NewRelease(arg)
-   if e != nil {
-      panic(e)
-   }
-   var artists string
-   for _, artist := range album.ArtistCredit { artists += artist.Name + " " }
-   for _, media := range album.Media {
-      for _, track := range media.Tracks {
-         id, e := youtubeResult(artists + track.Title)
-         if e != nil {
-            panic(e)
-         }
-         info, e := youtube.Info(id)
-         if e != nil {
-            panic(e)
-         }
-         info.Views()
-         time.Sleep(100 * time.Millisecond)
-      }
-   }
 }
