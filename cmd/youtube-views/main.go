2454bb7fd533eae9dba4ea2c49d1edf020673f45
Steven Penny <srpen6@gmail.com>
2021-04-30 14:57:17 -0500

    +   "strings"

--- cmd/youtube-views/main.go
+++ cmd/youtube-views/main.go
@@ -1,74 +1,26 @@
 package main
 
 import (
-   "bytes"
-   "fmt"
-   "github.com/89z/rosso"
-   "github.com/89z/rosso/musicbrainz"
-   "github.com/89z/youtube"
-   "net/http"
-   "net/url"
    "os"
-   "regexp"
-   "time"
+   "strings"
 )
 
 func main() {
    if len(os.Args) != 2 {
-      println("youtube-views <URL>")
-      os.Exit(1)
-   }
-   arg := os.Args[1]
-   addr, err := url.Parse(arg)
-   if err != nil {
-      panic(err)
-   }
-   id := addr.Query().Get("v")
-   video, err := youtube.NewVideo(id)
-   if err != nil {
-      panic(err)
-   }
-   date, err := video.PublishDate()
-   if err != nil {
-      panic(err)
-   }
-   view := time.Since(date).Hours() / 24 / 365
-   if view > 10_000_000 {
-      rosso.LogFail("Fail", view)
-   } else {
-      rosso.LogPass("Pass", view)
-   }
-}
-
-func main() {
-   if len(os.Args) != 2 {
       println(`usage:
-musicbrainz-views <URL>
+youtube-views <URL>
 
 examples:
-https://musicbrainz.org/release-group/d03bb6b1-d7b4-38ea-974e-847cbb31dca4
-https://musicbrainz.org/release/7a629d52-6a61-3ea1-a0a0-dd50bdef63b4`)
+https://www.youtube.com/watch?v=6e5cNaU1h1I
+https://musicbrainz.org/release/7a629d52-6a61-3ea1-a0a0-dd50bdef63b4
+https://musicbrainz.org/release-group/d03bb6b1-d7b4-38ea-974e-847cbb31dca4`)
       os.Exit(1)
    }
    arg := os.Args[1]
-   album, e := musicbrainz.NewRelease(arg)
-   if e != nil {
-      panic(e)
-   }
-   var artists string
-   for _, artist := range album.ArtistCredit { artists += artist.Name + " " }
-   for _, media := range album.Media {
-      for _, track := range media.Tracks {
-         id, e := youtubeResult(artists + track.Title)
-         if e != nil {
-            panic(e)
-         }
-         info, e := youtube.Info(id)
-         if e != nil {
-            panic(e)
-         }
-         info.Views()
-         time.Sleep(100 * time.Millisecond)
-      }
+   switch {
+   case strings.HasPrefix(arg, "https://musicbrainz.org/"):
+      viewMusicbrainz(arg)
+   case strings.HasPrefix(arg, "https://www.youtube.com/"):
+      viewYouTube(arg)
    }
 }
--- cmd/youtube-views/views.go
+++ cmd/youtube-views/views.go
@@ -8,20 +8,19 @@ import (
    "github.com/89z/youtube"
    "net/http"
    "net/url"
-   "os"
    "regexp"
    "time"
 )
 
 func youtubeResult(query string) (string, error) {
-   req, e := http.NewRequest("GET", "http://youtube.com/results", nil)
-   if e != nil { return "", e }
+   req, err := http.NewRequest("GET", "http://youtube.com/results", nil)
+   if err != nil { return "", err }
    val := req.URL.Query()
    val.Set("search_query", query)
    req.URL.RawQuery = val.Encode()
    rosso.LogInfo("GET", req.URL)
-   res, e := new(http.Client).Do(req)
-   if e != nil { return "", e }
+   res, err := new(http.Client).Do(req)
+   if err != nil { return "", err }
    var buf bytes.Buffer
    buf.ReadFrom(res.Body)
    str := buf.String()
@@ -30,4 +29,40 @@ func youtubeResult(query string) (string, error) {
       return "", fmt.Errorf("%v", req.URL)
    }
    return find[1], nil
+}
+
+func viewMusicbrainz(addr string) error {
+   album, err := musicbrainz.NewRelease(arg)
+   if err != nil { return err }
+   var artists string
+   for _, artist := range album.ArtistCredit { artists += artist.Name + " " }
+   for _, media := range album.Media {
+      for _, track := range media.Tracks {
+         id, err := youtubeResult(artists + track.Title)
+         if err != nil { return err }
+         info, err := youtube.Info(id)
+         if err != nil { return err }
+         info.Views()
+         time.Sleep(100 * time.Millisecond)
+      }
+   }
+   return nil
+}
+
+func viewYouTube(addr string) error {
+   addr, err := url.Parse(arg)
+   if err != nil { return err }
+   id := addr.Query().Get("v")
+   video, err := youtube.NewVideo(id)
+   if err != nil { return err }
+   date, err := video.PublishDate()
+   if err != nil {
+      panic(err)
+   }
+   view := time.Since(date).Hours() / 24 / 365
+   if view > 10_000_000 {
+      rosso.LogFail("Fail", view)
+   } else {
+      rosso.LogPass("Pass", view)
+   }
 }
