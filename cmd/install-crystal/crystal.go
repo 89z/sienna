49cec2e27121fd53f8ae81a063db61d97d3d0d1c
Steven Penny <srpen6@gmail.com>
2021-05-16 22:19:12 -0500

    new file:   cmd/install-crystal/crystal.go

---
+++ cmd/install-crystal/crystal.go
@@ -0,0 +1,11 @@
+package main
+
+/*
+.\crystal.exe build hello.cr
+
+# Error: can't find file 'prelude'
+$env:CRYSTAL_PATH = 'D:\Desktop\etc\crystal\src'
+
+# LINK : fatal error LNK1181: cannot open input file 'pcre.lib'
+$env:LIB += ';D:\Desktop\etc'
+*/
---
+++ cmd/install-crystal/readme.md
@@ -0,0 +1,3 @@
+# Install Crystal
+
+https://www.youtube.com/watch?v=eAzIAjTBGgU
