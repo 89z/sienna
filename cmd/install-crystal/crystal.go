b10d8f9409676c1e7787a8d1839557628cde8672
Steven Penny <srpen6@gmail.com>
2021-05-16 23:41:05 -0500

    +func getCreate(get, create string) error {

--- cmd/install-crystal/crystal.go
+++ cmd/install-crystal/crystal.go
@@ -1,11 +1,47 @@
 package main
 
-/*
-.\crystal.exe build hello.cr
+import (
+   "os"
+   "path/filepath"
+)
 
-# Error: can't find file 'prelude'
-$env:CRYSTAL_PATH = 'D:\Desktop\etc\crystal\src'
+func getCreate(get, create string) error {
+   _, err := os.Stat(create)
+   if err == nil {
+      fmt.Println(invert, "Exist", reset, create)
+      return nil
+   }
+   fmt.Println(invert, "Get", reset, get)
+   res, err := http.Get(get)
+   if err != nil { return err }
+   defer res.Body.Close()
+   os.Mkdir(filepath.Dir(create), os.ModeDir)
+   file, err := os.Create(create)
+   if err != nil { return err }
+   defer file.Close()
+   _, err = file.ReadFrom(res.Body)
+   return err
+}
 
-# LINK : fatal error LNK1181: cannot open input file 'pcre.lib'
-$env:LIB += ';D:\Desktop\etc'
-*/
+func main() {
+   // cache
+   cache, err := os.UserCacheDir()
+   if err != nil {
+      panic(err)
+   }
+   cache = filepath.Join(cache, "sienna/crystal")
+   // get
+   err = getCreate(
+      "https://codeload.github.com/crystal-lang/crystal/zip/refs/heads/master",
+      filepath.Join(cache, "crystal-master.zip"),
+   )
+   if err != nil {
+      panic(err)
+   }
+   // create
+   create := filepath.Join(cache, "crystal.zip")
+   // get
+   getCreate(
+   https://github.com/crystal-lang/crystal/suites/2754559253/artifacts/60912181
+   )
+}
