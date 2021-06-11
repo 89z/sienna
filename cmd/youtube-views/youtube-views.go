feb5a46d417ecd618dfda831bfb17dd4c1372102
Steven Penny <srpen6@gmail.com>
2021-06-11 00:02:00 -0500

    renamed:    cmd/youtube-views/views.go -> cmd/youtube-views/youtube-views.go

--- cmd/youtube-views/youtube-views.go
+++ cmd/youtube-views/youtube-views.go
@@ -7,7 +7,10 @@ import (
    "io"
    "net/http"
    "net/url"
+   "os"
+   "path"
    "regexp"
+   "strings"
    "time"
 )
 
@@ -69,15 +72,18 @@ func viewYouTube(addr string) error {
 }
 
 func youtubeResult(query string) (string, error) {
-   req, err := http.NewRequest("GET", "http://youtube.com/results", nil)
+   req, err := http.NewRequest("GET", "https://www.youtube.com/results", nil)
    if err != nil { return "", err }
    val := req.URL.Query()
    val.Set("search_query", query)
    req.URL.RawQuery = val.Encode()
    fmt.Println(invert, "GET", reset, req.URL)
-   res, err := new(http.Client).Do(req)
+   res, err := new(http.Transport).RoundTrip(req)
    if err != nil { return "", err }
    defer res.Body.Close()
+   if res.StatusCode != http.StatusOK {
+      return "", fmt.Errorf("Status %v", res.Status)
+   }
    body, err := io.ReadAll(res.Body)
    if err != nil { return "", err }
    re := regexp.MustCompile("/vi/([^/]+)/")
@@ -86,4 +92,42 @@ func youtubeResult(query string) (string, error) {
       return "", fmt.Errorf("FindSubmatch %v", re)
    }
    return string(find[1]), nil
+}
+
+func main() {
+   if len(os.Args) != 2 {
+      fmt.Println(`youtube-views <URL>
+
+https://www.youtube.com/watch?v=6e5cNaU1h1I
+https://musicbrainz.org/release/7a629d52-6a61-3ea1-a0a0-dd50bdef63b4
+https://musicbrainz.org/release-group/d03bb6b1-d7b4-38ea-974e-847cbb31dca4`)
+      return
+   }
+   arg := os.Args[1]
+   if strings.Contains(arg, "youtube.com/") {
+      err := viewYouTube(arg)
+      if err != nil {
+         panic(err)
+      }
+      return
+   }
+   id := path.Base(arg)
+   if strings.Contains(arg, "musicbrainz.org/release/") {
+      r, err := musicbrainz.NewRelease(id)
+      if err != nil {
+         panic(err)
+      }
+      if err := viewMusicbrainz(r); err != nil {
+         panic(err)
+      }
+      return
+   }
+   g, err := musicbrainz.NewGroup(id)
+   if err != nil {
+      panic(err)
+   }
+   g.Sort()
+   if err := viewMusicbrainz(g.Releases[0]); err != nil {
+      panic(err)
+   }
 }
