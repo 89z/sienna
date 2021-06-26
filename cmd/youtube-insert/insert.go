00a39959e7cab0371814c2cf4d6bfe72d4cfcfc1
Steven Penny <srpen6@gmail.com>
2021-06-26 16:17:04 -0500

    new file:   cmd/youtube-insert/newrow.go

--- cmd/youtube-insert/insert.go
+++ cmd/youtube-insert/insert.go
@@ -5,73 +5,23 @@ import (
    "flag"
    "fmt"
    "github.com/89z/mech/youtube"
-   "net/http"
+   "math/big"
    "net/url"
    "os"
-   "regexp"
-   "strconv"
-   "strings"
-   "time"
 )
 
-func httpHead(addr string) bool {
-   resp, err := http.Head(addr)
-   return err == nil && resp.StatusCode == http.StatusOK
+const (
+   invert = "\x1b[7m"
+   reset = "\x1b[m"
+)
+
+func mod(a, b int64) int64 {
+   c, d := big.NewInt(a), big.NewInt(b)
+   return new(big.Int).Mod(c, d).Int64()
 }
 
-func newTableRow(enc string) (*tableRow, error) {
-   dec, err := url.Parse(enc)
-   if err != nil {
-      return nil, err
-   }
-   id := dec.Query().Get("v")
-   // year
-   video, err := youtube.NewWeb(id)
-   if err != nil {
-      return nil, err
-   }
-   if video.ShortDescription == "" {
-      return nil, fmt.Errorf("clapham Junction")
-   }
-   year := video.PublishDate[:4]
-   for _, pat := range []string{
-      /* the order doesnt matter here, as we will find the lowest date of all
-      matches */
-      ` (\d{4})`, `(\d{4}) `, `Released on: (\d{4})`, `℗ (\d{4})`,
-   } {
-      re := regexp.MustCompile(pat).FindStringSubmatch(video.ShortDescription)
-      if re == nil {
-         continue
-      }
-      if re[1] < year {
-         year = re[1]
-      }
-   }
-   // song, artist
-   re := regexp.MustCompile(".* · .*").FindString(video.ShortDescription)
-   if re != "" {
-      titles := strings.Split(re, " · ")
-      artists := titles[1:]
-      video.Title = strings.Join(artists, ", ") + " - " + titles[0]
-   }
-   // time
-   now := strconv.FormatInt(time.Now().Unix(), 36)
-   // print
-   val := make(url.Values)
-   val.Set("a", now)
-   val.Set("b", id)
-   val.Set("p", "y")
-   val.Set("y", year)
-   if ! httpHead("http://i.ytimg.com/vi/" + id + "/sddefault.jpg") {
-      if httpHead("http://i.ytimg.com/vi/" + id + "/sd1.jpg") {
-         val.Set("c", "sd1")
-      } else {
-         val.Set("c", "hqdefault")
-      }
-   }
-   return &tableRow{
-      val.Encode(), video.Title,
-   }, nil
+func score(i youtube.Image) int64 {
+   return mod(480 - i.Height, 720) + i.Frame + i.Format
 }
 
 type tableRow struct {
@@ -88,10 +38,11 @@ func main() {
       flag.PrintDefaults()
       return
    }
-   row, err := newTableRow(flag.Arg(0))
+   addr, err := url.Parse(flag.Arg(0))
    if err != nil {
       panic(err)
    }
+   row, err := newTableRow(addr.Query().Get("v"))
    fmt.Printf("%#v\n", row)
    if dry {
       return
---
+++ cmd/youtube-insert/newrow.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+   "fmt"
+   "github.com/89z/mech/youtube"
+   "net/http"
+   "net/url"
+   "path"
+   "regexp"
+   "sort"
+   "strconv"
+   "strings"
+   "time"
+)
+
+func newTableRow(id string) (*tableRow, error) {
+   val := make(url.Values)
+   val.Set("p", "y")
+   val.Set("b", id)
+   // image
+   sort.SliceStable(youtube.Images, func(d, e int) bool {
+      return score(youtube.Images[d]) < score(youtube.Images[e])
+   })
+   for n, img := range youtube.Images {
+      addr := img.Address(id)
+      fmt.Println(invert, "Head", reset, addr)
+      res, err := http.Head(addr)
+      if err == nil && res.StatusCode == http.StatusOK {
+         if n > 0 {
+            val.Set("c", path.Base(addr))
+         }
+         break
+      }
+   }
+   // year
+   video, err := youtube.NewWeb(id)
+   if err != nil {
+      return nil, err
+   }
+   if video.ShortDescription == "" {
+      return nil, fmt.Errorf("clapham Junction")
+   }
+   year := video.PublishDate[:4]
+   for _, pat := range []string{
+      /* the order doesnt matter here, as we will find the lowest date of all
+      matches */
+      ` (\d{4})`, `(\d{4}) `, `Released on: (\d{4})`, `℗ (\d{4})`,
+   } {
+      re := regexp.MustCompile(pat).FindStringSubmatch(video.ShortDescription)
+      if re == nil {
+         continue
+      }
+      if re[1] < year {
+         year = re[1]
+      }
+   }
+   val.Set("y", year)
+   // song, artist
+   re := regexp.MustCompile(".* · .*").FindString(video.ShortDescription)
+   if re != "" {
+      titles := strings.Split(re, " · ")
+      artists := titles[1:]
+      video.Title = strings.Join(artists, ", ") + " - " + titles[0]
+   }
+   // time
+   now := strconv.FormatInt(time.Now().Unix(), 36)
+   val.Set("a", now)
+   // return
+   return &tableRow{
+      val.Encode(), video.Title,
+   }, nil
+}
